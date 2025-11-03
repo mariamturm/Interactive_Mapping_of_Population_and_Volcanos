de51138f2a2035e57ac0e858e6b5c663de0f6036
mariam <mariam@kompishka.local>
2025-11-03 12:03:28 +0400

    Adding markers feature: integrate folium markers

--- map1.py
+++ map1.py
@@ -1,4 +1,12 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Webmap_datasources/Volcanoes.txt")
+latitude = list(data["LAT"])
+longitude = list(data["LON"])
+#name = list(data["NAME"])
+
+
 map = folium.Map(
     location = [38.58, -99.09], 
     zoom_start = 6, 
@@ -10,8 +18,13 @@ fg = folium.FeatureGroup(name = "My Map")
 #fg.add_child(folium.Marker(location = [38.2, -99.1], popup = "Hi I am a Marker", icon = folium.Icon(color = 'green')))
 #fg.add_child(folium.Marker(location = [37.2, -89.1], popup = "Hi I am a Marker", icon = folium.Icon(color = 'green')))
 
-for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
-    fg.add_child(folium.Marker(location = coordinates, popup = "Hi I am a Marker", icon = folium.Icon(color = 'green')))
+for lt, ln in zip(latitude, longitude):
+        fg.add_child(folium.Marker(location = [lt, ln], popup = "Hi, this is marker", icon = folium.Icon(color = 'green')))
+
+
+
+# for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
+#     fg.add_child(folium.Marker(location = coordinates, popup = "Hi I am a Marker", icon = folium.Icon(color = 'green')))
 
 
 map.add_child(fg)
