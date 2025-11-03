f30c1ddd09a9409e5f9fcbd21bad59f02086a6d9
mariam <mariam@kompishka.local>
2025-11-03 12:13:33 +0400

    Adding more volacnos and cleaning up the code

--- map1.py
+++ map1.py
@@ -11,20 +11,11 @@ map = folium.Map(
     location = [38.58, -99.09], 
     zoom_start = 6, 
     tiles = "CartoDB positron",
-    #tiles = "Stamen Terrain",
-    #attr='Map tiles by Stamen Design, CC BY 3.0 — Map data © OpenStreetMap'
     )
 fg = folium.FeatureGroup(name = "My Map")
-#fg.add_child(folium.Marker(location = [38.2, -99.1], popup = "Hi I am a Marker", icon = folium.Icon(color = 'green')))
-#fg.add_child(folium.Marker(location = [37.2, -89.1], popup = "Hi I am a Marker", icon = folium.Icon(color = 'green')))
 
 for lt, ln, nm in zip(latitude, longitude, name):
         fg.add_child(folium.Marker(location = [lt, ln], popup = "This is " + nm + " volcano", icon = folium.Icon(color = 'green')))
-
-
-
-# for coordinates in [[38.2, -99.1],[39.2, -97.1]]:
-#     fg.add_child(folium.Marker(location = coordinates, popup = "Hi I am a Marker", icon = folium.Icon(color = 'green')))
 
 
 map.add_child(fg)
